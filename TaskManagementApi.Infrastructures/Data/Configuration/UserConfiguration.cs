39028368c643faceeacdf0966ccbc52a4902f184
ProgrammerNoob <sungshigun@gmail.com>
2025-06-19 06:11:20 +0800

    feat: Adding TaskManagementDbContext and ConfigurationFile

---
+++ TaskManagementApi.Infrastructures/Data/Configuration/UserConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagement.Infrastructures.Identity;
+
+namespace TaskManagement.Infrastructures.Data.Configuration
+{
+    sealed partial class UserConfiguration : IEntityTypeConfiguration<ApplicationUsers>
+    {
+        public void Configure(EntityTypeBuilder<ApplicationUsers> builder)
+        {
+            
+        }
+    }
+}
--- TaskManagementApi.Infrastructures/Data/TaskManagementDbContext.cs
+++ TaskManagementApi.Infrastructures/Data/TaskManagementDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,5 +13,20 @@ namespace TaskManagement.Infrastructures.Data
 {
     public class TaskManagementDbContext : IdentityDbContext<ApplicationUsers,ApplicationRole,Guid>
     {
+        public DbSet<ApplicationUsers> UserApplicationDb { get; set; }
+        public DbSet<Tasks> TaskDb { get; set; }
+        public DbSet<Category> CategoryDb { get; set; }
+
+        public TaskManagementDbContext(DbContextOptions<TaskManagementDbContext> options): base(options) { }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            
+        }
+
+
+
     }
+
 }
