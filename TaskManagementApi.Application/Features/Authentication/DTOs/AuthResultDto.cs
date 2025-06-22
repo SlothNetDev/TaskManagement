eff832946df85aa9f8ec08e9d9d94008d7542f56
ProgrammerNoob <sungshigun@gmail.com>
2025-06-23 07:36:33 +0800

    feat: Create AuthResultDto for Login and create Account Result

---
+++ TaskManagementApi.Application/Features/Authentication/DTOs/AuthResultDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagementApi.Application.Features.Authentication.DTOs
+{
+    public record AuthResultDto(
+        string Tokenn,
+        DateTime ExpiresAt,
+        string UserName,
+        string Role
+        );
+}
