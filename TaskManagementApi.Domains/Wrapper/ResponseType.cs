c222d49c5a47fac082dd419f0b2c305e2616eba4
ProgrammerNoob <sungshigun@gmail.com>
2025-06-22 15:55:39 +0800

    feat: Add response type Api

---
+++ TaskManagementApi.Domains/Wrapper/ResponseType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagementApi.Domains.Wrapper
+{
+    public class ResponseType<T>
+    {
+        public bool Success { get; set; }
+        public string? Message { get; set; }
+        public T? Data { get; set; }
+        public List<string>? Errors { get; set; }
+
+        public static ResponseType<T> SuccessResult(T data, string message = "") =>
+            new ResponseType<T> { Success = true, Data = data, Message = message };
+
+        public static ResponseType<T> Failure(List<string> errors, string message = "") =>
+            new ResponseType<T> { Success = false, Errors = errors, Message = message };
+    }
+}
