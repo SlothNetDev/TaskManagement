46c512e8e5a0e0acd306c751fc2f385a28a49697
ProgrammerNoob <sungshigun@gmail.com>
2025-06-23 16:15:48 +0800

    feat: Adding seed roles in request pipeline

--- TaskManagementApi.PresentationUI/Extensions/ApplicationBuilderExtensions.cs
+++ TaskManagementApi.PresentationUI/Extensions/ApplicationBuilderExtensions.cs
@@ -1,8 +1,13 @@
-﻿namespace TaskManagementApi.PresentationUI.Extensions
+﻿using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using TaskManagement.Infrastructures.Data.Seeders;
+using TaskManagement.Infrastructures.Identity;
+
+namespace TaskManagementApi.PresentationUI.Extensions
 {
     public static class ApplicationBuilderExtensions
     {
-        public static WebApplication ConfigureApplication(this WebApplication app)
+        public static async Task<WebApplication> ConfigureApplication(this WebApplication app)
         {
              // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
@@ -10,6 +15,14 @@
                 app.MapOpenApi();
             }
 
+            //Seed Roles for request
+            using(var scope = app.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var roleManager = services.GetRequiredService<RoleManager<ApplicationRole>>();
+
+                await IdentitySeeder.SeedRolesAsync(roleManager);
+            }
             app.UseHttpsRedirection();
 
 
