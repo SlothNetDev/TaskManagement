9dc4bf469e4a062532a6f0e1f53819a31b375c9f
ProgrammerNoob <sungshigun@gmail.com>
2025-06-23 16:09:29 +0800

    feat: Adding IdentitySeeder to seed role

---
+++ TaskManagementApi.Infrastructures/Data/Seeders/IdentitySeeder.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagement.Infrastructures.Identity;
+
+namespace TaskManagement.Infrastructures.Data.Seeders
+{
+    public class IdentitySeeder
+    {
+         /// <summary>
+        /// Seeds predefined roles to the database if they don't exist.
+        /// </summary>
+        public static async Task SeedRolesAsync(RoleManager<ApplicationRole> roleManager)
+        {
+            string[] roles = new[] { "Admin", "User" };
+
+            foreach (var role in roles)
+            {
+                // Check if role exists
+                if (!await roleManager.RoleExistsAsync(role))
+                {
+                    // Create it if not
+                    await roleManager.CreateAsync(new ApplicationRole { Name = role });
+                }
+            }
+        }
+    }
+}
