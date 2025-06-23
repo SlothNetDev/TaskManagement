ed733efe0418476f8681a8c6c86f406671cd8f69
ProgrammerNoob <sungshigun@gmail.com>
2025-06-24 05:48:33 +0800

    feat: adding register and login action method in AuthController

---
+++ TaskManagementApi.PresentationUI/Controllers/AuthController.cs
@@ -0,0 +1,50 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TaskManagementApi.Application.Common.Interfaces.IAuthentication;
+using TaskManagementApi.Application.Features.Authentication.Commands;
+using TaskManagementApi.Application.Features.Authentication.Commands.Login;
+using TaskManagementApi.Application.Features.Authentication.DTOs;
+
+namespace TaskManagementApi.PresentationUI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        private readonly ILogger _logger;
+        public AuthController(IMediator mediator,ILogger logger)
+        {
+            _mediator = mediator;
+            _logger = logger;
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register([FromBody] UserRegisterRequestDto request)
+        {
+            var result = await _mediator.Send(new RegisterCommand(request));
+            if (!result.Success)
+            {
+                _logger.LogInformation("Register Failed, Ruquest is Null");
+                return BadRequest(result);
+            }
+            return Ok(result);
+                
+            
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login([FromBody] UserLoginRequestDto request)
+        {
+            var result = await _mediator.Send(new LoginCommand(request));
+            if (!result.Success)
+            {
+                _logger.LogInformation("Login Failed, Ruquest is Null");
+                return BadRequest(result);
+            }
+            return Ok(result);
+                
+            
+        }
+        
+    }
+}
