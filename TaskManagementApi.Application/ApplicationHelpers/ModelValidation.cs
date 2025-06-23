eb6a6775212e6cbc03aa674820e2a9fb807b835d
ProgrammerNoob <sungshigun@gmail.com>
2025-06-23 08:23:17 +0800

    feat: Add Model ValidationHelper

---
+++ TaskManagementApi.Application/ApplicationHelpers/ModelValidation.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using TaskManagementApi.Domains.Wrapper;
+
+namespace TaskManagementApi.Application.ApplicationHelpers
+{
+    internal class ModelValidation
+    {
+        public static List<string?> ModelValidationResponse<T>(T instance)
+        {
+            ResponseType<T> response = new();
+            if (instance == null)
+            {
+                response.Success = false;
+                response.Message = "Models are null";
+                response.Errors?.Add("Models Cannot be Null");
+                return response.Errors ?? new List<string>() {"error"};
+            }
+            List<ValidationResult> validationResults = new ();
+
+            var validationContext = new ValidationContext(instance);
+            Validator.TryValidateObject(instance, validationContext, validationResults, true);
+            return validationResults.Select(x => x.ErrorMessage).ToList();
+
+        }
+    }
+}
