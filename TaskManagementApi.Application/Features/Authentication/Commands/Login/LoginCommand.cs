c3c1be55dbac9962ae6e54474027efb3028fa3e9
ProgrammerNoob <sungshigun@gmail.com>
2025-06-25 05:04:35 +0800

    configure: combining command and commandHandler

--- TaskManagementApi.Application/Features/Authentication/Commands/Login/LoginCommand.cs
+++ TaskManagementApi.Application/Features/Authentication/Commands/Login/LoginCommand.cs
@@ -1,5 +1,6 @@
 ﻿
 using MediatR;
+using TaskManagementApi.Application.Common.Interfaces.IAuthentication;
 using TaskManagementApi.Application.Features.Authentication.DTOs;
 using TaskManagementApi.Domains.Wrapper;
 
@@ -12,4 +13,12 @@ namespace TaskManagementApi.Application.Features.Authentication.Commands.Login
     /// cannot be null.</param>
     public record LoginCommand(UserLoginRequestDto Dto) : IRequest<ResponseType<AuthResultDto>>;
 
+    //Processing Request
+    public class LoginCommandHandler(IIdentityService _identity) : IRequestHandler<LoginCommand, ResponseType<AuthResultDto>>
+    {
+        public async Task<ResponseType<AuthResultDto>> Handle(LoginCommand request, CancellationToken cancellationToken)
+        {
+            return await _identity.LoginAsync(request.Dto);
+        }
+    }
 }
--- TaskManagementApi.Application/Features/Authentication/Commands/Register/RegisterCommand.cs
+++ TaskManagementApi.Application/Features/Authentication/Commands/Register/RegisterCommand.cs
@@ -1,9 +1,21 @@
 ﻿
 using MediatR;
+using TaskManagementApi.Application.Common.Interfaces.IAuthentication;
 using TaskManagementApi.Application.Features.Authentication.DTOs;
 using TaskManagementApi.Domains.Wrapper;
 
 namespace TaskManagementApi.Application.Features.Authentication.Commands
 {
+    //Sending request 
     public record RegisterCommand(UserRegisterRequestDto dto) : IRequest<ResponseType<AuthResultDto>>;
+
+    //Handling Request
+    public class RegisterCommandHandler(IIdentityService _identity) : IRequestHandler<RegisterCommand, ResponseType<AuthResultDto>>
+    {
+        public async Task<ResponseType<AuthResultDto>> Handle(RegisterCommand request, CancellationToken cancellationToken)
+        {
+            return await _identity.RegisterAsync(request.dto);
+        }
+    }
+
 }
