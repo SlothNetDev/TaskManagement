ed9b57ed69b5571a4f99a76f174ad36619c11c87
ProgrammerNoob <sungshigun@gmail.com>
2025-06-26 05:10:10 +0800

    fix: Changing ItokenService GenerateRefreshTokenFrom AuthResultDto Into RefreshTokenResponseDto

--- TaskManagementApi.Application/Common/Interfaces/IAuthentication/ITokenService.cs
+++ TaskManagementApi.Application/Common/Interfaces/IAuthentication/ITokenService.cs
@@ -12,6 +12,6 @@ namespace TaskManagementApi.Application.Common.Interfaces.IAuthentication
     public interface ITokenService
     {
         Task<AuthResultDto> GenerateTokenAsync(TokenUserDto user); 
-        AuthResponseDto GenerateRefreshToken();
+        RefreshTokenResponseDto GenerateRefreshToken();
     }
 }
