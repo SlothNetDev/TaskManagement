28392633cfa90001fba67590d90d9ee55919cfc5
ProgrammerNoob <sungshigun@gmail.com>
2025-06-26 04:07:10 +0800

    fix: fixing IIdentity Interface and Adding GetRefreshToken

--- TaskManagementApi.Application/Common/Interfaces/IAuthentication/IIdentityService.cs
+++ TaskManagementApi.Application/Common/Interfaces/IAuthentication/IIdentityService.cs
@@ -8,11 +8,33 @@ using TaskManagementApi.Domains.Wrapper;
 
 namespace TaskManagementApi.Application.Common.Interfaces.IAuthentication
 {
-    public interface IIdentityService
+    public interface IIdentityService<TRefreshTokenDto>
     {
-        Task<ResponseType<RefreshTokenResponseDto>> RefreshTokenAsync(string token, string refreshToken);
+        /// <summary>
+        /// Interface For Login Account Async
+        /// </summary>
+        /// <param name="dto"></param>
+        /// <returns>AuthResultDto response Type</returns>
         Task<ResponseType<AuthResultDto>> LoginAsync(UserLoginRequestDto dto);
+        /// <summary>
+        /// Interface for Register Account Async
+        /// </summary>
+        /// <param name="dto"></param>
+        /// <returns>AuthResult Dto Response Type</returns>
         Task<ResponseType<AuthResultDto>> RegisterAsync(UserRegisterRequestDto dto);
 
+        /// <summary>
+        /// Generating Refresh Token for an Account
+        /// </summary>
+        /// <param name="token"></param>
+        /// <param name="refreshToken"></param>
+        /// <returns>RefreshTokenResponseDto </returns>
+        Task<ResponseType<AuthResultDto>> RefreshTokenAsync(string token, string refreshToken);
+        /// <summary>
+        /// Get all RefreshToken of Accounts
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns>List of Account RefreshToken</returns>
+        Task<ResponseType<List<TRefreshTokenDto>>> GetRefreshTokenAsync(Guid userId);
     }
 }
