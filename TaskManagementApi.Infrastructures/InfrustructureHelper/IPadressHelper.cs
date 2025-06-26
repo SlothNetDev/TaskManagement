70ca7c3368d0ed1265d8c0dcde5f057ec5f4d284
ProgrammerNoob <sungshigun@gmail.com>
2025-06-26 16:22:27 +0800

    Configure: changing IPAdress location method in seperate file

---
+++ TaskManagementApi.Infrastructures/InfrustructureHelper/IPadressHelper.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace TaskManagement.Infrastructures.InfrustructureHelper
+{
+    internal class IPadressHelper(IHttpContextAccessor _httpContextAccessor)
+    {
+        public  string GetIpAddress()
+        {
+            return _httpContextAccessor.HttpContext?.Connection?.RemoteIpAddress?.ToString() ?? "Unknown";
+        }
+           
+    }
+}
