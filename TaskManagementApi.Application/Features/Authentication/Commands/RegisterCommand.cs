505fe1d029d071f0895edcb3c0e2e9d066da5b38
ProgrammerNoob <sungshigun@gmail.com>
2025-06-26 18:45:19 +0800

    configure: Change return type of  register and command handler with sting

--- TaskManagementApi.Application/Features/Authentication/Commands/RegisterCommand.cs
+++ TaskManagementApi.Application/Features/Authentication/Commands/RegisterCommand.cs
@@ -7,12 +7,12 @@ using TaskManagementApi.Domains.Wrapper;
 namespace TaskManagementApi.Application.Features.Authentication.Commands
 {
     //Sending request 
-    public record RegisterCommand(UserRegisterRequestDto dto) : IRequest<ResponseType<AuthResultDto>>;
+    public record RegisterCommand(UserRegisterRequestDto dto) : IRequest<ResponseType<string>>;
 
     //Handling Request
-    public class RegisterCommandHandler(IAuthService _identity) : IRequestHandler<RegisterCommand, ResponseType<AuthResultDto>>
+    public class RegisterCommandHandler(IAuthService _identity) : IRequestHandler<RegisterCommand, ResponseType<string>>
     {
-        public async Task<ResponseType<AuthResultDto>> Handle(RegisterCommand request, CancellationToken cancellationToken)
+        public async Task<ResponseType<string>> Handle(RegisterCommand request, CancellationToken cancellationToken)
         {
             return await _identity.RegisterAsync(request.dto);
         }
