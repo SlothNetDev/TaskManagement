c2a705eae1785feed4201de182397182dc983786
ProgrammerNoob <sungshigun@gmail.com>
2025-06-26 15:57:29 +0800

    feat: Adding LogOutCommand and CommandHandler

--- TaskManagementApi.Application/Features/Authentication/Commands/LogoutCommand.cs
+++ TaskManagementApi.Application/Features/Authentication/Commands/LogoutCommand.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaskManagementApi.Application.Common.Interfaces.IAuthentication;
+using TaskManagementApi.Application.Features.Authentication.DTOs;
+using TaskManagementApi.Domains.Wrapper;
 
 namespace TaskManagementApi.Application.Features.Authentication.Commands
 {
-    internal class LogoutCommand
+    //Processing Request
+    public record LogOutCommand(LogOutRequestDto dto) : IRequest<ResponseType<string>>;
+
+    //Handling request
+    public class LogoutCommandHandler(
+        IRefreshTokenService _refreshTokenService // Use an interface defined in Application
+    ) : IRequestHandler<LogOutCommand, ResponseType<string>>
     {
+        public async Task<ResponseType<string>> Handle(LogOutCommand request, CancellationToken cancellationToken)
+        {
+            // Delegate to the service inside Infrastructure
+            return await _refreshTokenService.LogoutAsync(request.dto);
+        }
     }
+
 }
