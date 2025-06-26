a6ab1733344c083ab88b78ef10fbfd9590647547
ProgrammerNoob <sungshigun@gmail.com>
2025-06-27 00:24:33 +0800

    Revert "Rearange: Putting all User"
    This reverts commit 27f65ea21c397ee798ae2f5da588a3f4a1c445ec.

---
+++ TaskManagementApi.Application/Features/Authentication/DTOs/AuthResultDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagementApi.Application.Features.Authentication.DTOs
+{
+    public record AuthResultDto(
+        string Token,
+        DateTime ExpiresAt,
+        string RefreshToken,
+        string UserName,
+        string Role
+        );
+}
