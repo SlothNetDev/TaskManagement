5b5dca86fa1a4c207f39eaa0bd7bfe92758e932d
ProgrammerNoob <sungshigun@gmail.com>
2025-06-28 05:29:41 +0800

    fix: chaning location of all TaskDto into Task\TaskDto

---
+++ TaskManagementApi.Application/Features/Task/TaskDto/TaskUpdateDto.cs
@@ -0,0 +1,25 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+using TaskManagementApi.Domains.Enums;
+
+namespace TaskManagementApi.Application.DTOs.TaskDto
+{
+    public record TaskUpdateDto(
+        [Required(ErrorMessage = "Id is Required")]
+        Guid Id,
+
+        [Required(ErrorMessage = "Title is Required")]
+        [StringLength(120, ErrorMessage = "Title Cannot exceed 120 characters")]
+        string? Title,
+        Priority? Priority,
+        Status? Status)
+    {
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public Priority? Priority { get; } = Priority;
+    
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public Status? Status { get; init; } = Status;
+        
+    };
+}
