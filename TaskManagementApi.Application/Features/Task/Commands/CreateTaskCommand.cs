5c641221e19ebbd283f731b85877b5c80edfcbe5
ProgrammerNoob <sungshigun@gmail.com>
2025-06-27 21:24:56 +0800

    feat: Creating TaskCommand and commandHandler

---
+++ TaskManagementApi.Application/Features/Task/Commands/CreateTaskCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using TaskManagementApi.Application.Common.Interfaces.ITask;
+using TaskManagementApi.Application.DTOs.TaskDto;
+using TaskManagementApi.Domains.Wrapper;
+namespace TaskManagementApi.Application.Features.Task.Commands
+{
+    public record CreateTaskCommand(TaskRequestDto createDto) : IRequest<ResponseType<TaskResponseDto>>;
+    public class CreateTaskCommandHandler(ICreateTask task) : IRequestHandler<CreateTaskCommand, ResponseType<TaskResponseDto>>
+    {
+        public async Task<ResponseType<TaskResponseDto>> Handle(CreateTaskCommand request, CancellationToken cancellationToken)
+        {
+            return await task.CreateTaskAsync(request.createDto);
+        }
+    }
+}
