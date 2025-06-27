0213d2c89816f4e799070e44235e861c0557993f
ProgrammerNoob <sungshigun@gmail.com>
2025-06-28 05:58:28 +0800

    feat: adding Update TaskCommand and CommandHandler

---
+++ TaskManagementApi.Application/Features/Task/Commands/UpdateTaskCommand.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using TaskManagementApi.Application.Common.Interfaces.ITaskItem.TaskCommand;
+using TaskManagementApi.Application.DTOs.TaskDto;
+using TaskManagementApi.Domains.Wrapper;
+
+namespace TaskManagementApi.Application.Features.Task.Commands
+{
+    public record UpdateTaskCommand(TaskUpdateDto dto) :IRequest<ResponseType<TaskResponseDto>>;
+    public class UpdateTaskCommandHandler(IUpdateTaskService service) : IRequestHandler<UpdateTaskCommand, ResponseType<TaskResponseDto>>
+    {
+        public async Task<ResponseType<TaskResponseDto>> Handle(UpdateTaskCommand request, CancellationToken cancellationToken)
+        {
+            return await service.UpdateTaskAsync(request.dto);
+        }
+    }
+}
