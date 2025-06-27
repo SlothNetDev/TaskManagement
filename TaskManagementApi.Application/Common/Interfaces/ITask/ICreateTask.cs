138e7a85b7316e6144afb2df26cfedeed89e2e80
ProgrammerNoob <sungshigun@gmail.com>
2025-06-27 21:24:36 +0800

    feat: Adding Create Task Interface

---
+++ TaskManagementApi.Application/Common/Interfaces/ITask/ICreateTask.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagementApi.Application.DTOs.TaskDto;
+using TaskManagementApi.Domains.Entities;
+using TaskManagementApi.Domains.Wrapper;
+
+namespace TaskManagementApi.Application.Common.Interfaces.ITask
+{
+    public interface ICreateTask
+    {
+        Task<ResponseType<TaskResponseDto>> CreateTaskAsync(TaskRequestDto request);
+    }
+}
