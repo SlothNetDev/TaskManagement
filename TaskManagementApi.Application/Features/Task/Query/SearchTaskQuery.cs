c8d41e988994a19be1424162d98c352671c5d7fb
ProgrammerNoob <sungshigun@gmail.com>
2025-06-28 18:22:34 +0800

    feat: Adding SearchTaskQuery Command and command handler

---
+++ TaskManagementApi.Application/Features/Task/Query/SearchTaskQuery.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagementApi.Application.Common.Interfaces.ITaskItem.TaskQuery;
+using TaskManagementApi.Application.DTOs.TaskDto;
+using TaskManagementApi.Domains.Wrapper;
+
+namespace TaskManagementApi.Application.Features.Task.Query
+{
+    public record SearchTaskQuery() : IRequest<ResponseType<List<TaskResponseDto>>>;
+
+    public class SearchTaskQueryHandler(ISearchTask task) : IRequestHandler<SearchTaskQuery, ResponseType<List<TaskResponseDto>>>
+    {
+        public async Task<ResponseType<List<TaskResponseDto>>> Handle(SearchTaskQuery request, CancellationToken cancellationToken)
+        {
+            return await task.SearchTaskAsync();
+        }
+    }
+}
