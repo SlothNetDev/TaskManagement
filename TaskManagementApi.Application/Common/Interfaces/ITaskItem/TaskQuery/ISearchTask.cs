aff82f9bdfee0ecd2b6199b363fd093cddcf38d4
ProgrammerNoob <sungshigun@gmail.com>
2025-06-28 18:13:15 +0800

    feat: Adding ISearchTask Interface

---
+++ TaskManagementApi.Application/Common/Interfaces/ITaskItem/TaskQuery/ISearchTask.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagementApi.Application.DTOs.TaskDto;
+
+namespace TaskManagementApi.Application.Common.Interfaces.ITaskItem.TaskQuery
+{
+    public interface ISearchTask
+    {
+        Task<List<TaskResponseDto>> SearchTaskAsync();
+    }
+}
