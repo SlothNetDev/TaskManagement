87d8fc767518f02b045aa63050fdfa08db34e9f9
ProgrammerNoob <sungshigun@gmail.com>
2025-06-28 18:22:01 +0800

    fix: fixing IsearchTask to return responseType list

--- TaskManagementApi.Application/Common/Interfaces/ITaskItem/TaskQuery/ISearchTask.cs
+++ TaskManagementApi.Application/Common/Interfaces/ITaskItem/TaskQuery/ISearchTask.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TaskManagementApi.Application.DTOs.TaskDto;
+﻿using TaskManagementApi.Application.DTOs.TaskDto;
+using TaskManagementApi.Domains.Wrapper;
 
 namespace TaskManagementApi.Application.Common.Interfaces.ITaskItem.TaskQuery
 {
     public interface ISearchTask
     {
-        Task<List<TaskResponseDto>> SearchTaskAsync();
+        Task<ResponseType<List<TaskResponseDto>>> SearchTaskAsync();
     }
 }
