eb15c0c5e6eb181364619bf46c5550ecc58c9b17
ProgrammerNoob <sungshigun@gmail.com>
2025-06-28 18:12:43 +0800

    fix: GetAllTaskCommand and commandHanlder

--- TaskManagementApi.Application/Features/Task/Query/GetTaskQuery.cs
+++ TaskManagementApi.Application/Features/Task/Query/GetTaskQuery.cs
@@ -10,11 +10,11 @@ using TaskManagementApi.Domains.Wrapper;
 
 namespace TaskManagementApi.Application.Features.Task.Query
 {
-    public record GetAllTaskQuery : IRequest<ResponseType<TaskResponseDto>>;
+    public record GetAllTaskQuery : IRequest<ResponseType<List<TaskResponseDto>>>;
 
-    public class GetAllTaskQueryHandler(IGetAllTask getTask) : IRequestHandler<GetAllTaskQuery, ResponseType<TaskResponseDto>>
+    public class GetAllTaskQueryHandler(IGetAllTask getTask) : IRequestHandler<SearchTaskQuery, ResponseType<List<TaskResponseDto>>>
     {
-        public async Task<ResponseType<TaskResponseDto>> Handle(GetAllTaskQuery request, CancellationToken cancellationToken)
+        public async Task<ResponseType<List<TaskResponseDto>>> Handle(SearchTaskQuery request, CancellationToken cancellationToken)
         {
             return await getTask.GetAllTaskAsync();
         }
