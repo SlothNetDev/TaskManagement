db5dc1908672ff4e029b936d2891e3cd9cd261f3
ProgrammerNoob <sungshigun@gmail.com>
2025-06-29 09:37:51 +0800

    feat: Adding query handler and request mediaR for paganation

---
+++ TaskManagementApi.Application/Features/Task/Query/GetPaganationTasksQuery.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagementApi.Application.Common.Interfaces.ITaskItem.TaskQuery;
+using TaskManagementApi.Application.DTOs.TaskDto;
+using TaskManagementApi.Application.Features.Task.TaskDto;
+using TaskManagementApi.Core.Wrapper;
+using TaskManagementApi.Domains.Wrapper;
+
+namespace TaskManagementApi.Application.Features.Task.Query
+{
+    public record GetPaganationTasksQuery(PaganationDto dto) :IRequest<ResponseType<PaganationResponse<TaskResponseDto>>>;
+
+    public class GetPaganationTasksQueryHandler(IPaganationTaskService service) : IRequestHandler<GetPaganationTasksQuery, ResponseType<PaganationResponse<TaskResponseDto>>>
+    {
+        public Task<ResponseType<PaganationResponse<TaskResponseDto>>> Handle(GetPaganationTasksQuery request, CancellationToken cancellationToken)
+        {
+            return service.PaganationAsync(request.dto);
+        }
+    }
+}
