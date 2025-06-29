9c3b49ef76f59b2e62107f81b65ec79107144a56
ProgrammerNoob <sungshigun@gmail.com>
2025-06-29 09:06:48 +0800

    feat: adding delete command and commandHandler

---
+++ TaskManagementApi.Application/Features/Task/Commands/DeleteTaskCommand.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using TaskManagementApi.Application.Common.Interfaces.ITaskItem.TaskCommand;
+using TaskManagementApi.Application.DTOs.TaskDto;
+using TaskManagementApi.Domains.Wrapper;
+
+namespace TaskManagementApi.Application.Features.Task.Commands
+{
+    public record DeleteTaskCommand(Guid id) : IRequest<ResponseType<TaskResponseDto>>;
+
+    public class DeleteTaskCommandHandler(IDeleteTaskService deleteTask) : IRequestHandler<DeleteTaskCommand, ResponseType<TaskResponseDto>>
+    {
+        public async Task<ResponseType<TaskResponseDto>> Handle(DeleteTaskCommand request, CancellationToken cancellationToken)
+        {
+            return await deleteTask.DeleteTaskAsync(request.id);
+        }
+    }
+}
