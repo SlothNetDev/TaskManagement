2cfec4f60cc66d751225d106b1bdd25d6b38b2c4
ProgrammerNoob <sungshigun@gmail.com>
2025-06-30 14:11:59 +0800

    feat: Add get all categories interface

---
+++ TaskManagementApi.Application/Common/Interfaces/ICategory/CategoryQuery/IGetAllCategories.cs
@@ -0,0 +1,10 @@
+﻿using TaskManagementApi.Application.Features.CategoryFeature.CategoriesDto;
+using TaskManagementApi.Domains.Wrapper;
+
+namespace TaskManagementApi.Application.Common.Interfaces.ICategory.CategoryQuery
+{
+    public interface IGetAllCategories
+    {
+        Task<ResponseType<List<CategoryResponseDtoWithTask>>> GetAllCategoriesAsync();
+    }
+}
