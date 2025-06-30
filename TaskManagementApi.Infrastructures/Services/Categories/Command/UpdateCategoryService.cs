86f7bdf4900375853f0e91c73b41fa2989549820
ProgrammerNoob <sungshigun@gmail.com>
2025-06-30 14:52:25 +0800

    feat: Adding update category service

---
+++ TaskManagementApi.Infrastructures/Services/Categories/Command/UpdateCategoryService.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagement.Infrastructures.Data;
+using TaskManagement.Infrastructures.Services.TaskService.Command;
+using TaskManagementApi.Application.ApplicationHelpers;
+using TaskManagementApi.Application.Common.Interfaces.ICategory.CategoryCommand;
+using TaskManagementApi.Application.Features.CategoryFeature.CategoriesDto;
+using TaskManagementApi.Domains.Wrapper;
+
+namespace TaskManagement.Infrastructures.Services.Categories.Command
+{
+    public class UpdateCategoryService(
+        TaskManagementDbContext _dbContext,
+        ILogger<CreateTaskService> _logger,
+        IHttpContextAccessor _httpContextAccessor) : IUpdateCategoryService
+    {
+        public async Task<ResponseType<CategoryResponseDto>> UpdateCategoriesAsync(CategoryUpdateDto requestDto)
+        {
+            var response = new ResponseType<CategoryResponseDto>();
+
+            //1. Validate user
+            var validationErrors = ModelValidation.ModelValidationResponse(requestDto);
+
+            //check if all field  request was correct
+            if (validationErrors.Any())
+            {
+                _logger.LogWarning("Request validation failed for {Endpoint}. Errors: {@ValidationErrors}", 
+                "POST /login", 
+                validationErrors);
+                response.Success = false;
+                response.Message = "Field Request for Models has an Error";
+                return response;
+            };
+
+             //2. Get user for JWT
+            var userId = _httpContextAccessor.HttpContext?.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if(string.IsNullOrWhiteSpace(userId) || !Guid.TryParse(userId, out var parseUserId))
+            {
+                _logger.LogWarning("Failed to extract valid user ID {id} from JWT.",userId);
+                response.Success = false;
+                response.Message = "Unauthorized or invalid user.";
+                return response;
+            }
+            //check id categories if exist
+            var updateCategory = await _dbContext.CategoryDb.FirstOrDefaultAsync(id => id.UserId == parseUserId);
+            //check if categoryId is exist
+            if(updateCategory is null)
+            {
+                _logger.LogWarning($"{updateCategory} Id Cannot Found");
+                response.Success = false;
+                response.Message = "Blog not found";
+                return response;
+            }
+            try
+            {
+                //update category
+                updateCategory.CategoryName = requestDto.CategoryName ?? string.Empty;
+                updateCategory.Description = requestDto.Description ?? string.Empty;
+
+                //save the update
+                await _dbContext.SaveChangesAsync();
+                response.Success = true;
+                response.Data = new CategoryResponseDto(
+                    updateCategory.Id,
+                    updateCategory.CategoryName,
+                    updateCategory.Description);
+                return response;
+            }
+            catch(Exception ex)
+            {
+                _logger.LogInformation("Categories Update Failed from user {user}, Reason: {reason}", updateCategory.Id,ex.Message);
+                response.Success = false;
+                response.Message = "Failed to Update Categories";
+                return response;
+            }
+        }
+    }
+}
