93ab5cf81a32cd0b67f39445ba49734025208b6d
ProgrammerNoob <sungshigun@gmail.com>
2025-06-30 14:12:19 +0800

    feat: Adding category response with list of task

--- TaskManagementApi.Application/Features/CategoryFeature/CategoriesDto/CategoryResponseDto.cs
+++ TaskManagementApi.Application/Features/CategoryFeature/CategoriesDto/CategoryResponseDto.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaskManagementApi.Application.DTOs.TaskDto;
 using TaskManagementApi.Domains.Entities;
 
 namespace TaskManagementApi.Application.Features.CategoryFeature.CategoriesDto
@@ -11,12 +12,10 @@ namespace TaskManagementApi.Application.Features.CategoryFeature.CategoriesDto
     public record CategoryResponseDto(
         Guid Id,      
         string CategoryName,   
-        string Description)
-    {
-        public CategoryResponseDto(Category category) : this(
-            category.Id,
-            category.CategoryName,
-            category.Description ?? string.Empty)
-        { }
-    }
+        string Description);
+    public record CategoryResponseDtoWithTask(
+        Guid Id,      
+        string CategoryName,   
+        string Description,
+        List<TaskResponseDto> Tasks);
 }
