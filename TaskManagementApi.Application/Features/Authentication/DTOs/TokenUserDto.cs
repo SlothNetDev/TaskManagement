c33f38a1124aab6856106b0dcd4452547a42c333
ProgrammerNoob <sungshigun@gmail.com>
2025-06-30 12:23:32 +0800

    fix: changes location of TokenUserDto

---
+++ TaskManagementApi.Application/Features/Authentication/DTOs/TokenUserDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagementApi.Application.Features.Authentication.DTOs
+{
+    public record TokenUserDto(
+        string UserId,
+        string UserName,
+        string Email,
+        List<string> Roles
+        );
+}
