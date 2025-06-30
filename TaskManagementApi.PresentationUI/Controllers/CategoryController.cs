6286849859549fa6c97ef9155c7f99ed1d64840e
ProgrammerNoob <sungshigun@gmail.com>
2025-06-30 14:24:04 +0800

    feat: Add get all categores controller

--- TaskManagementApi.PresentationUI/Controllers/CategoryController.cs
+++ TaskManagementApi.PresentationUI/Controllers/CategoryController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using TaskManagementApi.Application.Features.CategoryFeature.CategoriesDto;
 using TaskManagementApi.Application.Features.CategoryFeature.Commands;
+using TaskManagementApi.Application.Features.CategoryFeature.Queries;
 
 namespace TaskManagementApi.PresentationUI.Controllers
 {
@@ -20,6 +21,17 @@ namespace TaskManagementApi.PresentationUI.Controllers
             if (!result.Success)
             {
                 _logger.LogWarning("Creating {categoryName} Failed", result.Data.CategoryName);
+                return BadRequest(result);
+            }
+            return Ok(result);
+        }
+        [HttpPost("getAll-category")]
+        public async Task<IActionResult> GetAllCategoryAsync()
+        {
+            var result = await _mediaR.Send(new GetAllCategoriesQuery());
+            if (!result.Success)
+            {
+                _logger.LogWarning("Successfully Retrieve all Categories");
                 return BadRequest(result);
             }
             return Ok(result);
