1d4a4a0a3d9c6c9938a916d10163f80013b6f33b
ProgrammerNoob <sungshigun@gmail.com>
2025-06-30 15:16:14 +0800

    feat: Adding delete category Command and command handler

---
+++ TaskManagementApi.Application/Features/CategoryFeature/Commands/DeleteCategoryCommand.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagementApi.Application.Common.Interfaces.ICategory.CategoryCommand;
+using TaskManagementApi.Application.Features.CategoryFeature.CategoriesDto;
+using TaskManagementApi.Domains.Wrapper;
+
+namespace TaskManagementApi.Application.Features.CategoryFeature.Commands
+{
+    public record DeleteCategoryCommand(Guid Id) :IRequest<ResponseType<CategoryResponseDto>>;
+    public class DeleteCategoryCommandHandler(IDeleteCategoryService service) : IRequestHandler<DeleteCategoryCommand, ResponseType<CategoryResponseDto>>
+    {
+        public async Task<ResponseType<CategoryResponseDto>> Handle(DeleteCategoryCommand request, CancellationToken cancellationToken)
+        {
+            return await service.DeleteCategoryAsync(request.Id);
+        }
+    }
+}
