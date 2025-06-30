26283e3b8ef3a884749f6ae381d486c13d9847d9
ProgrammerNoob <sungshigun@gmail.com>
2025-07-01 01:24:06 +0800

    fix: Dto for creating task

--- TaskManagementApi.Application/Features/Task/TaskDto/TaskRequestDto.cs
+++ TaskManagementApi.Application/Features/Task/TaskDto/TaskRequestDto.cs
@@ -1,11 +1,15 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
+using TaskManagementApi.Domains.Entities;
 using TaskManagementApi.Domains.Enums;
 
 namespace TaskManagementApi.Application.DTOs.TaskDto
 {
     public class TaskRequestDto
     {
+        [Required(ErrorMessage = "Category Id is required")]
+        public Guid CategoryId { get; set; }
+
         [Required(ErrorMessage = "Title is Required")]
         [StringLength(120, ErrorMessage = "Title Cannot exceed 120 characters")]
         public string Title { get; set; } = string.Empty;
@@ -15,5 +19,13 @@ namespace TaskManagementApi.Application.DTOs.TaskDto
 
         [Required(ErrorMessage = "Decide a Due Date")]
         public DateTime DueDate { get; set; }
+
+        public TaskRequestDto(TaskItem item)
+        {
+            CategoryId = item.CategoryId;
+            Title = item.Title;
+            Priority = item.Priority;
+            DueDate = item.DueDate ?? DateTime.UtcNow.AddDays(1); // make due date as 1 day if not given
+        }
     }
 }
