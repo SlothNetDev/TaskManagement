072c843b4af08b6c84ae4e8434aad67482f102c9
ProgrammerNoob <sungshigun@gmail.com>
2025-06-30 14:51:59 +0800

    feat: Adding update category command and command handler

---
+++ TaskManagementApi.Application/Features/CategoryFeature/Commands/UpdateCategoryCommand.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using TaskManagementApi.Application.Common.Interfaces.ICategory.CategoryCommand;
+using TaskManagementApi.Application.Features.CategoryFeature.CategoriesDto;
+using TaskManagementApi.Domains.Wrapper;
+
+namespace TaskManagementApi.Application.Features.CategoryFeature.Commands
+{
+    public record UpdateCategoryCommand(CategoryUpdateDto dto): IRequest<ResponseType<CategoryResponseDto>>;
+
+    public class UpdateCategoryCommandHandler(IUpdateCategoryService service) : IRequestHandler<UpdateCategoryCommand, ResponseType<CategoryResponseDto>>
+    {
+        public async Task<ResponseType<CategoryResponseDto>> Handle(UpdateCategoryCommand request, CancellationToken cancellationToken)
+        {
+            return await service.UpdateCategoriesAsync(request.dto);
+        }
+    }
+}
