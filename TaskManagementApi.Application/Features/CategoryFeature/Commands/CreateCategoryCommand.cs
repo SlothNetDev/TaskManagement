479e49d2244e7ff104cd3a4a581b3572ad8a7905
ProgrammerNoob <sungshigun@gmail.com>
2025-06-30 12:25:05 +0800

    feat: Adding category create command and command handler

---
+++ TaskManagementApi.Application/Features/CategoryFeature/Commands/CreateCategoryCommand.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using TaskManagementApi.Application.Common.Interfaces.ICategory.CategoryCommand;
+using TaskManagementApi.Application.Features.CategoryFeature.CategoriesDto;
+using TaskManagementApi.Domains.Wrapper;
+
+namespace TaskManagementApi.Application.Features.CategoryFeature.Commands
+{
+    public record CreateCategoryCommand(CategoryRequestDto dto) :IRequest<ResponseType<CategoryResponseDto>>;
+
+    public class CreateCategoryCommandHandler(ICreateCategoryService create) : IRequestHandler<CreateCategoryCommand, ResponseType<CategoryResponseDto>>
+    {
+        public Task<ResponseType<CategoryResponseDto>> Handle(CreateCategoryCommand request, CancellationToken cancellationToken)
+        {
+            return create.CreateCategoryAsync(request.dto);
+        }
+    }
+}
