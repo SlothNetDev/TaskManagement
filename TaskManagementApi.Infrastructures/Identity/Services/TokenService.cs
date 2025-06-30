6097e76536625610c9d339638d32ff932d3182ee
ProgrammerNoob <sungshigun@gmail.com>
2025-06-30 12:26:31 +0800

    fix: changes location of tokenService not interface

--- TaskManagementApi.Infrastructures/Identity/Services/TokenService.cs
+++ TaskManagementApi.Infrastructures/Identity/Services/TokenService.cs
@@ -10,7 +10,6 @@ using TaskManagement.Infrastructures.Identity.Models;
 using TaskManagement.Infrastructures.InfrustructureHelper;
 using TaskManagementApi.Application.Common.Interfaces.IAuthentication;
 using TaskManagementApi.Application.Common.Settings;
-using TaskManagementApi.Application.DTOs;
 using TaskManagementApi.Application.Features.Authentication.DTOs;
 using TaskManagementApi.Domains.Wrapper;
 
