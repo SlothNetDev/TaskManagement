832f71a90b8bef15816d44a7e03965edea292a1b
ProgrammerNoob <sungshigun@gmail.com>
2025-06-30 12:23:55 +0800

    fix: changes location of all categoryDto

---
+++ TaskManagementApi.Application/Features/CategoryFeature/CategoriesDto/CategoryRequestDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagementApi.Domains.Entities;
+
+namespace TaskManagementApi.Application.Features.CategoryFeature.CategoriesDto
+{
+    public record CategoryRequestDto(
+        [Required(ErrorMessage = "Category Name is Required")]
+        [StringLength(120, ErrorMessage = "Category Name Cannot exceed with 120 characters")]
+        string CategoryName,
+        [StringLength(1000, ErrorMessage = "Descriptions Cannot exceed with 1000 characters")]
+        string? Description
+        );
+}
---
+++ TaskManagementApi.Application/Features/CategoryFeature/CategoriesDto/CategoryResponseDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagementApi.Domains.Entities;
+
+namespace TaskManagementApi.Application.Features.CategoryFeature.CategoriesDto
+{
+    public record CategoryResponseDto(
+        Guid Id,      
+        string CategoryName,   
+        string Description)
+    {
+        public CategoryResponseDto(Category category) : this(
+            category.Id,
+            category.CategoryName,
+            category.Description ?? string.Empty)
+        { }
+    }
+}
