2b064e77b456e5127e40dce79b86bb47ac306a4a
ProgrammerNoob <sungshigun@gmail.com>
2025-06-30 12:23:00 +0800

    feat: Adding createcategoryInterface

---
+++ TaskManagementApi.Application/Common/Interfaces/ICategory/CategoryCommand/ICreateCategoryService.cs
@@ -0,0 +1,10 @@
+﻿using TaskManagementApi.Application.Features.CategoryFeature.CategoriesDto;
+using TaskManagementApi.Domains.Wrapper;
+
+namespace TaskManagementApi.Application.Common.Interfaces.ICategory.CategoryCommand
+{
+    public interface ICreateCategoryService
+    {
+        Task<ResponseType<CategoryResponseDto>> CreateCategoryAsync(CategoryRequestDto requestDto);
+    }
+}
