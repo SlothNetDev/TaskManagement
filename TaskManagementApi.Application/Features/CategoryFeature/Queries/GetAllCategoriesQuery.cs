9a4a773cb444841885a38d550555091adfa82bb9
ProgrammerNoob <sungshigun@gmail.com>
2025-06-30 14:12:55 +0800

    feat: adding query for get all task query and queryHandler

---
+++ TaskManagementApi.Application/Features/CategoryFeature/Queries/GetAllCategoriesQuery.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using TaskManagementApi.Application.Common.Interfaces.ICategory.CategoryQuery;
+using TaskManagementApi.Application.Common.Interfaces.ITaskItem.TaskQuery;
+using TaskManagementApi.Application.Features.CategoryFeature.CategoriesDto;
+using TaskManagementApi.Domains.Wrapper;
+namespace TaskManagementApi.Application.Features.CategoryFeature.Queries
+{
+    public record GetAllCategoriesQuery() : IRequest<ResponseType<List<CategoryResponseDtoWithTask>>>;
+    public class GetAllCategoriesQueryHanlder(IGetAllCategories dto) : IRequestHandler<GetAllCategoriesQuery, ResponseType<List<CategoryResponseDtoWithTask>>>
+    {
+        public async Task<ResponseType<List<CategoryResponseDtoWithTask>>> Handle(GetAllCategoriesQuery request, CancellationToken cancellationToken)
+        {
+            return await dto.GetAllCategoriesAsync();
+        }
+    }
+}
