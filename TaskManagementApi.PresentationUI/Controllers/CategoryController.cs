c38a9850a3b177f1765bb40b13b0d201621f3390
ProgrammerNoob <sungshigun@gmail.com>
2025-06-30 12:41:47 +0800

    feat: Adding add category controller

---
+++ TaskManagementApi.PresentationUI/Controllers/CategoryController.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TaskManagementApi.Application.Features.CategoryFeature.CategoriesDto;
+using TaskManagementApi.Application.Features.CategoryFeature.Commands;
+
+namespace TaskManagementApi.PresentationUI.Controllers
+{
+    [Route("category")]
+    [ApiController]
+    [Authorize]
+    public class CategoryController(ILogger<TaskItemController> _logger,
+        Mediator _mediaR) : ControllerBase
+    {
+        [HttpPost("create-category")]
+        public async Task<IActionResult> CreateCategoryAsync([FromBody] CategoryRequestDto request)
+        {
+            var result = await _mediaR.Send(new CreateCategoryCommand(request));
+            if (!result.Success)
+            {
+                _logger.LogWarning("Creating {categoryName} Failed", result.Data.CategoryName);
+                return BadRequest(result);
+            }
+            return Ok(result);
+        }
+    }
+}
