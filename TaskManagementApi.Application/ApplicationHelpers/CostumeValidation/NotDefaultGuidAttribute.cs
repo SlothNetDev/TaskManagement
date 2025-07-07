1a085235dc18ed41ac63e8f7980974c340cb59ca
ProgrammerNoob <sungshigun@gmail.com>
2025-07-08 05:39:03 +0800

    feat: Adding validation for Guid Empty

---
+++ TaskManagementApi.Application/ApplicationHelpers/CostumeValidation/NotDefaultGuidAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagementApi.Application.ApplicationHelpers.CostumeValidation
+{
+    internal class NotDefaultGuidAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object? value)
+        {
+            if (value is Guid guid)
+                return guid != Guid.Empty;
+            return false;
+        }
+    }
+}
