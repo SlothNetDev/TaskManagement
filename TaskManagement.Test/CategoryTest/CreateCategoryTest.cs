1d2e67362ae1fd07f006163749d93d0699515651
ProgrammerNoob <sungshigun@gmail.com>
2025-07-09 09:18:51 +0800

    conifgure: create category test confige and shortening

--- TaskManagement.Test/CategoryTest/CreateCategoryTest.cs
+++ TaskManagement.Test/CategoryTest/CreateCategoryTest.cs
@@ -7,6 +7,7 @@ using Moq;
 using TaskManagement.Infrastructures.Data;
 using TaskManagement.Infrastructures.Identity.Models;
 using TaskManagement.Infrastructures.Services.Categories.Command;
+using TaskManagement.Infrastructures.Services.Categories.Query;
 using TaskManagement.Infrastructures.Services.TaskService.Command;
 using TaskManagement.Test.HelperTest;
 using TaskManagementApi.Application.Common.Interfaces.ICategory.CategoryCommand;
@@ -21,39 +22,41 @@ namespace TaskManagement.Test.CategoryTest
 {
     public class CreateCategoryTest /*: IDisposable*/
     {
-        private readonly ITestOutputHelper _output;
-        private readonly Mock<ApplicationDbContext> _mockDbContext;
-
-        private readonly ICreateCategoryService _categoriesService;
+         private readonly ITestOutputHelper _output;
+        private Mock<ApplicationDbContext> _mockDbContext;
 
         private Guid _jwtUserId = Guid.NewGuid();
-
-        private CategoryRequestDto _validRequest;
-        private readonly ApplicationUsers _testUser = null!;
+        private readonly ApplicationUsers _applicationUser;
         public CreateCategoryTest(ITestOutputHelper output)
         {
             _output = output;
-
             //initialize test data
-            _testUser = HelperTestMethod.CreateTestUser(_jwtUserId);
-            //setUp mocks
-            _mockDbContext = HelperTestMethod.CreateMockDbContext(_testUser);
+            _applicationUser = HelperTestMethod.CreateTestUser(_jwtUserId);
+              
 
-            //httContextAccessor
-            _categoriesService = CreateServiceUnderTest();
         }
-        public CreateCategoryService CreateServiceUnderTest()
+        private Mock<ApplicationDbContext> GetMockDbContext(ApplicationUsers _applicationUser,Category _category)
         {
-            var mockLogger = new Mock<ILogger<CreateCategoryService>>();
-            var mockHttpContextAccessor = new Mock<IHttpContextAccessor>();
-            
-            HelperTestMethod.SetUpHttpContextAccessor(_jwtUserId, mockHttpContextAccessor);
-            
-            return new CreateCategoryService(
-                _mockDbContext.Object,
-                mockLogger.Object,
-                mockHttpContextAccessor.Object
-            );
+             // Set up DbContextMock
+           var dbContext = new DbContextMock<ApplicationDbContext>(new DbContextOptions<ApplicationDbContext>());
+            _mockDbContext = dbContext;
+            dbContext.CreateDbSetMock(x => x.UserApplicationDb, new[] { _applicationUser });
+            dbContext.CreateDbSetMock(x => x.CategoryDb, new[] {_category});
+            _mockDbContext.Setup(x => x.SaveChangesAsync(It.IsAny<CancellationToken>())).ReturnsAsync(1);
+
+            return dbContext;
+        }
+        private CreateCategoryService CreateCategoriesUnderTest(Category categories)
+        {
+             var _mockLogger = new Mock<ILogger<CreateCategoryService>>();
+             var _mockHttpContextAccessor = new Mock<IHttpContextAccessor>();
+
+            HelperTestMethod.SetUpHttpContextAccessor(_jwtUserId, _mockHttpContextAccessor);
+
+            _mockDbContext = GetMockDbContext(_applicationUser, categories);
+
+            return new CreateCategoryService(_mockDbContext.Object,
+                _mockLogger.Object, _mockHttpContextAccessor.Object);
         }
             
         /// <summary>
@@ -63,35 +66,49 @@ namespace TaskManagement.Test.CategoryTest
         [Fact]
         public async Task CreateCategories_WithValidData_Should_CreateSuccessfully()
         {
+            var dto = new CategoryRequestDto
+            {
+                CategoryName = "mahana",
+                Description = "shogano"
+            };
+            var createCategory = HelperTestMethod.CreateTestCategories( _applicationUser, _output,dto);
+            
+            var service = CreateCategoriesUnderTest(createCategory);
             AsserApiHelpers assert = new(_output);
             //create request category
-            _validRequest = new CategoryRequestDto
-            {
-                CategoryName = "My love life",
-                Description = "bwahalalaa"
-            };
-            var result = await _categoriesService.CreateCategoryAsync(_validRequest);
+
+            var result = await service.CreateCategoryAsync(dto);
 
             assert.ShouldSucceed(result, "Category Created Successfully");
             assert.ShouldMatch(result, data =>
-                data.CategoryName == _validRequest.CategoryName &&
-                data.Description == _validRequest.Description);
+                data.CategoryName == result.Data.CategoryName &&
+                data.Description == result.Data.Description);
         }
         [Fact]
-        public async Task CreateCatgories_WithBoundaryValues_Should_CreateSuccessfully()
+        public async Task CreateEntity_Should_ReturnValidationError_WhenFieldExceedsMaxLength()
         {
-            AsserApiHelpers assert = new(_output);
-            _validRequest = new CategoryRequestDto
+            var dto = new CategoryRequestDto
             {
-                CategoryName = new string('a', 120), //great value
-                Description = new string('a', 1000) //exact value
+                CategoryName = new string('c',140),
+                Description = new string('c',1500)
             };
-            var result = await _categoriesService.CreateCategoryAsync(_validRequest);
+            var createCategory = HelperTestMethod.CreateTestCategories( _applicationUser, _output,dto);
+            
+            var service = CreateCategoriesUnderTest(createCategory);
+            AsserApiHelpers assert = new(_output);
+            //create request category
 
-            assert.ShouldSucceed(result, "Category Created Successfully");
-            assert.ShouldMatch(result, data =>
-                data.CategoryName == _validRequest.CategoryName &&
-                data.Description == _validRequest.Description);
+            var result = await service.CreateCategoryAsync(dto);
+
+            string expectedResponseMessage = "Invalid input. Please check the provided data";
+            var expectedValidationErrors = new List<string>
+            {
+                "Category Name Cannot exceed with 120 characters",
+                "Descriptions Cannot exceed with 1000 characters" // Adjust based on your actual Description error
+            };
+
+            assert.ShouldFail(result, expectedResponseMessage, expectedValidationErrors);
+            
         }
 
         
