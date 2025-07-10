8e6e697862d6c4ad560b62b55afba60dcede3938
ProgrammerNoob <sungshigun@gmail.com>
2025-07-10 18:35:42 +0800

    fix: Adding JsonProperty Name for AuthResultDto

--- TaskManagementApi.Application/Features/Authentication/DTOs/AuthResultDto.cs
+++ TaskManagementApi.Application/Features/Authentication/DTOs/AuthResultDto.cs
@@ -2,15 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace TaskManagementApi.Application.Features.Authentication.DTOs
 {
-    public record AuthResultDto(
-        string Token,
-        DateTime ExpiresAt,
-        string RefreshToken,
-        string UserName,
-        string Role
-        );
+    public class AuthResultDto
+    {
+        [JsonPropertyName("bearerToken")]
+        public string BearerToken { get; set; }
+        
+        [JsonPropertyName("expiresAt")]
+        public DateTime ExpiresAt { get; set; }
+        
+        [JsonPropertyName("refreshToken")]
+        public string RefreshToken { get; set; }
+        
+        [JsonPropertyName("userName")]
+        public string UserName { get; set; }
+        
+        [JsonPropertyName("role")]
+        public string Role { get; set; }
+    }
 }
