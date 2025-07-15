bf93876654a0f25cb7731a93223003868f83128e
slothnetdev <sungshigun@gmail.com>
2025-07-16 07:51:05 +0800

    feat: writing the delete Service On DeleteCategory service into DeleteCategoryCommand.cs

--- TaskManagementApi.Application/Features/CategoryFeature/Commands/DeleteCategoryCommand.cs
+++ TaskManagementApi.Application/Features/CategoryFeature/Commands/DeleteCategoryCommand.cs
@@ -4,18 +4,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 using TaskManagementApi.Application.Common.Interfaces.ICategory.CategoryCommand;
+using TaskManagementApi.Application.Common.Interfaces.Repository;
 using TaskManagementApi.Application.Features.CategoryFeature.CategoriesDto;
+using TaskManagementApi.Core.IRepository.Categories;
 using TaskManagementApi.Domains.Wrapper;
 
 namespace TaskManagementApi.Application.Features.CategoryFeature.Commands
 {
     public record DeleteCategoryCommand(Guid Id) :IRequest<ResponseType<CategoryResponseDto>>;
-    public class DeleteCategoryCommandHandler(IDeleteCategoryService service) : IRequestHandler<DeleteCategoryCommand, ResponseType<CategoryResponseDto>>
+    public class DeleteCategoryCommandHandler(ICategoryRepository dbContext,
+        IGetDomainIdCategoryRepository identityService,
+        ILogger<CreateCategoryCommandHandler> logger) : IRequestHandler<DeleteCategoryCommand, ResponseType<CategoryResponseDto>>
     {
         public async Task<ResponseType<CategoryResponseDto>> Handle(DeleteCategoryCommand request, CancellationToken cancellationToken)
         {
-            return await service.DeleteCategoryAsync(request.Id);
+            //get jwt user Id
+            var userDomainResponse = await identityService.GetCurrentUserDomainIdDeleteCategoryAsync(request.Id);
+            if (!userDomainResponse.Success)
+            {
+                logger.LogWarning("Failed to retrieve DomainUserId or category validation failed: {Message}", userDomainResponse.Message);
+                return ResponseType<CategoryResponseDto>.Fail(userDomainResponse.Message);
+            }
+            var categoryToDelete = userDomainResponse.Data;
+            
+            try
+            {
+                await dbContext.DeleteAsync(categoryToDelete);
+
+                logger.LogInformation("Successfully deleted category {categoryId}", categoryToDelete);
+                return ResponseType<CategoryResponseDto>.SuccessResult(
+                    new CategoryResponseDto(categoryToDelete),
+                    "Category deleted successfully");
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Error deleting category {categoryId}", categoryToDelete);
+                return ResponseType<CategoryResponseDto>.Fail(
+                    ex.Message,
+                    "Failed to delete category");
+            }
         }
     }
 }
