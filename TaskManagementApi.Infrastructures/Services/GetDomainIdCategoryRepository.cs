a0804463b57d149fc858f840c91da1a6a8960849
slothnetdev <sungshigun@gmail.com>
2025-07-16 07:52:19 +0800

    feat: Adding the GetDomainIdCategoryRepository delete

--- TaskManagementApi.Infrastructures/Services/GetDomainIdCategoryRepository.cs
+++ TaskManagementApi.Infrastructures/Services/GetDomainIdCategoryRepository.cs
@@ -87,4 +87,53 @@ public class GetDomainIdCategoryRepository(
         return ResponseType<Category>.SuccessResult(categoryToUpdate,
             "User Domain Id Retrieved Successfully");
     }
+
+    public async Task<ResponseType<Category>> GetCurrentUserDomainIdDeleteCategoryAsync(Guid id)
+    {
+        // 1. Get and validate user from JWT
+        var userId = httpContextAccessor.HttpContext?.User.FindFirstValue(ClaimTypes.NameIdentifier);
+        if (string.IsNullOrWhiteSpace(userId) || !Guid.TryParse(userId, out var parseUserId))
+        {
+            logger.LogWarning("Failed to extract valid user ID {id} from JWT.", userId);
+            return ResponseType<Category>.Fail("Unauthorized or invalid user.");
+        }
+        // 2. Match application user
+        var matchingApplicationUser = await applicationDbContext.UserApplicationDb
+            .FirstOrDefaultAsync(ac => ac.Id == parseUserId);
+        
+        if (matchingApplicationUser == null)
+        {
+            logger.LogWarning("No matching ApplicationUser found for userId {id}.", parseUserId);
+            return ResponseType<Category>.Fail("Invalid user account.");
+        }
+        
+        // 3. Validate domain user ID
+        var taskUserIdToUse = matchingApplicationUser.DomainUserId;
+        if (taskUserIdToUse == Guid.Empty)
+        {
+            logger.LogWarning("User {id} has an empty DomainUserId.", parseUserId);
+            return ResponseType<Category>.Fail("Invalid user configuration.");
+        }
+        
+        //check if Id exist
+        if (id == Guid.Empty)
+        {
+            logger.LogWarning("Delete {id} is not exist.", id);
+            return ResponseType<Category>.Fail("Invalid Id, It's not Exist");
+        }
+        // 4. Find and validate category
+        var categoryToDelete = await applicationDbContext.CategoryDb
+            .FirstOrDefaultAsync(c => c.UserId == taskUserIdToUse && c.Id == id);
+        
+        if (categoryToDelete == null)
+        {
+            logger.LogWarning("No category found with ID {categoryId} for user {userId}.", 
+                id, parseUserId);
+            return ResponseType<Category>.Fail("Category not found.");
+        }
+        
+        return ResponseType<Category>.SuccessResult(
+            categoryToDelete,
+            "User Domain Id Retrieved Successfully");
+    }
 }
