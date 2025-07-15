0dc78fce6aa0b7e81a1c67898875d09b4e82977a
slothnetdev <sungshigun@gmail.com>
2025-07-15 19:25:40 +0800

    feat: Adding CategoryRepository.cs

---
+++ TaskManagementApi.Infrastructures/Services/Categories/Command/CategoryRepository.cs
@@ -0,0 +1,37 @@
+﻿using TaskManagement.Infrastructures.Data;
+using TaskManagementApi.Core.IRepository.Categories;
+using TaskManagementApi.Domains.Entities;
+using TaskManagementApi.Domains.Wrapper;
+
+namespace TaskManagement.Infrastructures.Services.Categories.Command
+{
+    public class CategoryRepository(ApplicationDbContext dbContext ) :ICategoryRepository
+    {
+        public async Task<Category> CreateAsync(Category category)
+        {
+            await dbContext.CategoryDb.AddAsync(category);
+            await dbContext.SaveChangesAsync();
+            return  category;
+        }
+
+        public async Task<Category> DeleteAsync(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Category> UpdateAsync(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Category>> GetByAllCategoryAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Category> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
