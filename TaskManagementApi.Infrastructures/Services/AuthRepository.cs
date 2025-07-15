0f92b61118f1a5e1703c13db2e0e0d626e7aaa12
slothnetdev <sungshigun@gmail.com>
2025-07-16 05:12:19 +0800

    feat: adding GetApplicationUserIdCheckCategoryExist for create task

--- TaskManagementApi.Infrastructures/Services/AuthRepository.cs
+++ TaskManagementApi.Infrastructures/Services/AuthRepository.cs
@@ -16,17 +16,15 @@ public class AuthRepository(
     ILogger<AuthRepository> logger
     ) : IAuthRepository
 {
-    public async Task<ResponseType<Guid?>> GetCurrentUserDomainIdAsync()
+    public async Task<ResponseType<Guid>> GetCurrentUserDomainIdAsync()
     {
-        // 2. Get and validate user from JWT
         var userId = httpContextAccessor.HttpContext?.User.FindFirstValue(ClaimTypes.NameIdentifier);
         if (string.IsNullOrWhiteSpace(userId) || !Guid.TryParse(userId, out var parseUserId))
         {
             logger.LogWarning("Failed to extract valid user ID {id} from JWT.", userId);
-            return ResponseType<Guid?>.Fail("Unauthorized or invalid user.");
+            return ResponseType<Guid>.Fail("Unauthorized or invalid user.");
         }
         
-        // 3. Match application user
         logger.LogInformation("Attempting to create category for UserId: {UserId}", parseUserId);
         var matchingApplicationUser = await applicationDbContext.UserApplicationDb
             .FirstOrDefaultAsync(x => x.Id == parseUserId);
@@ -34,19 +32,59 @@ public class AuthRepository(
         if (matchingApplicationUser == null)
         {
             logger.LogWarning("No matching ApplicationUser found for userId {id}.", parseUserId);
-            return ResponseType<Guid?>.Fail("Invalid user account");
+            return ResponseType<Guid>.Fail("Invalid user account");
         }
         
-        // 4. Validate domain user ID
         var taskUserIdToUse = matchingApplicationUser.DomainUserId;
         if (taskUserIdToUse == Guid.Empty)
         {
             logger.LogWarning("User {id} has an empty DomainUserId.", parseUserId);
-            return ResponseType<Guid?>.Fail("Invalid user configuration");
+            return ResponseType<Guid>.Fail("Invalid user configuration");
         }
 
-        return ResponseType<Guid?>.SuccessResult(
-            taskUserIdToUse,
-            "Successfully GetUser Domain Id");
+        return ResponseType<Guid>.SuccessResult(matchingApplicationUser.DomainUserId,
+            "User Domain Id Retrieved Successfully");
+    }
+
+    public async Task<ResponseType<Guid>> GetApplicationUserIdAndCheckCategoryExistAysnc(Guid id)
+    {
+        var userId = httpContextAccessor.HttpContext?.User.FindFirstValue(ClaimTypes.NameIdentifier);
+        if (string.IsNullOrWhiteSpace(userId) || !Guid.TryParse(userId, out var parseUserId))
+        {
+            logger.LogWarning("Failed to extract valid user ID {id} from JWT.", userId);
+            return ResponseType<Guid>.Fail("Unauthorized or invalid user.");
+        }
+        
+        logger.LogInformation("Attempting to create category for UserId: {UserId}", parseUserId);
+        var matchingApplicationUser = await applicationDbContext.UserApplicationDb
+            .FirstOrDefaultAsync(x => x.Id == parseUserId);
+        
+        if (matchingApplicationUser == null)
+        {
+            logger.LogWarning("No matching ApplicationUser found for userId {id}.", parseUserId);
+            return ResponseType<Guid>.Fail("Invalid user account");
+        }
+        
+        var taskUserIdToUse = matchingApplicationUser.DomainUserId;
+        if (taskUserIdToUse == Guid.Empty)
+        {
+            logger.LogWarning("User {id} has an empty DomainUserId.", parseUserId);
+            return ResponseType<Guid>.Fail("Invalid user configuration");
+        }
+        //get category if it was exist in user
+        var category = await applicationDbContext.CategoryDb
+            .AnyAsync(x => x.Id == parseUserId);
+
+        //validate category
+        if (category is true)
+        {
+            logger.LogError("Expected {Category} was null when processing {Create}",
+                category,
+                "Create Task");
+            return ResponseType<Guid>.Fail("Category don't exist to user Account");
+        }
+
+        return ResponseType<Guid>.SuccessResult(matchingApplicationUser.DomainUserId,
+            "User Domain Id Retrieved Successfully");
     }
 }
