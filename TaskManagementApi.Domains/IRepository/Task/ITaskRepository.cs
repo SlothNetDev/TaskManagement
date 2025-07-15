b97f9a1d4ba39e328bd38e799f68d5468de81026
slothnetdev <sungshigun@gmail.com>
2025-07-16 07:54:57 +0800

    fix: changin the parameter type for ITaskRepository.cs

--- TaskManagementApi.Domains/IRepository/Task/ITaskRepository.cs
+++ TaskManagementApi.Domains/IRepository/Task/ITaskRepository.cs
@@ -6,8 +6,8 @@ public interface ITaskRepository
 {
     Task<TaskItem> CreateAsync(TaskItem taskItem);
     Task<TaskItem> UpdateAsync(TaskItem taskItem);
-    Task<TaskItem> DeleteAsync(TaskItem taskItem);
-    Task<TaskItem> GetByIdAsync(int id);
+    Task<TaskItem> DeleteAsync(TaskItem taskItemId);
+    Task<TaskItem> GetByIdAsync(TaskItem taskItemId);
     Task<IEnumerable<TaskItem>> GetAllAsync();
     Task<IEnumerable<TaskItem>> PaganationAsync(TaskItem taskItem, CancellationToken cancellationToken);
     Task<IEnumerable<TaskItem>> ISearchTask(string searchTerm);
