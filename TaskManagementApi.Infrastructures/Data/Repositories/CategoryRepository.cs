da4a4358ed8ad7558c21360777c87aea0ee67424
slothnetdev <sungshigun@gmail.com>
2025-07-17 05:00:39 +0800

    fix: change the location of category repository from TaskManagement.Infrastructures.Services.Categories into TaskManagement.Infrastructures.Data.Repositories

---
+++ TaskManagementApi.Infrastructures/Data/Repositories/CategoryRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManagement.Infrastructures.Data;
+using TaskManagementApi.Core.IRepository.Categories;
+using TaskManagementApi.Domains.Entities;
+using TaskManagementApi.Domains.Wrapper;
+
+namespace TaskManagement.Infrastructures.Data.Repositories
+{
+    public class CategoryRepository(ApplicationDbContext dbContext ) :ICategoryRepository
+    {
+        public async Task<Category> CreateAsync(Category category)
+        {
+            await dbContext.CategoryDb.AddAsync(category);
+            await dbContext.SaveChangesAsync();
+            return  category;
+        }
+
+        public async Task<Category> DeleteAsync(Category category)
+        {
+            dbContext.Remove(category);
+            await dbContext.SaveChangesAsync();
+            return category;
+        }
+
+        public async Task<Category> UpdateAsync(Category category)
+        {
+            dbContext.Update(category);
+            await dbContext.SaveChangesAsync();
+            return category;
+        }
+
+        public async Task<List<Category>> GetByAllCategoryAsync(Guid id)
+        {
+            return await dbContext.CategoryDb
+                .Where(x => x.UserId == id)
+                .Include(x => x.Tasks)
+                .ToListAsync();
+        }
+
+        public async Task<Category> GetByIdAsync(Guid id)
+        {
+            return await dbContext.CategoryDb
+                .Where(x => x.UserId == id)
+                .Include(x => x.Tasks)
+                .FirstOrDefaultAsync();
+        }
+    }
+}
