3fe6360c6e5046bc05bb46ff4adb1ab27eb0c273
slothnetdev <sungshigun@gmail.com>
2025-07-17 09:32:37 +0800

    feat: Adding delete in IGetDomainTaskRepository.cs

--- TaskManagementApi.Application/Common/Interfaces/Repository/IGetDomainTaskRepository.cs
+++ TaskManagementApi.Application/Common/Interfaces/Repository/IGetDomainTaskRepository.cs
@@ -7,4 +7,5 @@ public interface IGetDomainTaskRepository
 {
     Task<ResponseType<Guid>> GetCurrentUserDomainIdCreateTaskAsync();
     Task<ResponseType<TaskItem>> GetCurrentUserDomainIdUpdateTaskAsync(Guid id);
+    Task<ResponseType<TaskItem>> GetCurrentUserDomainIdDeleteTaskAsync(Guid id);
 }
