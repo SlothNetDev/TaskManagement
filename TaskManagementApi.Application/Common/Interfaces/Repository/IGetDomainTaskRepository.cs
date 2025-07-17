6ba0c5fdeb5f05bcbc7c5dd4f53a6d8260700836
slothnetdev <sungshigun@gmail.com>
2025-07-17 08:55:31 +0800

    feat: Adding GetCurrentUserDomainIdUpdateTaskAsync in IGetDomainTaskRepository

--- TaskManagementApi.Application/Common/Interfaces/Repository/IGetDomainTaskRepository.cs
+++ TaskManagementApi.Application/Common/Interfaces/Repository/IGetDomainTaskRepository.cs
@@ -1,3 +1,4 @@
+using TaskManagementApi.Domains.Entities;
 using TaskManagementApi.Domains.Wrapper;
 
 namespace TaskManagementApi.Application.Common.Interfaces.Repository;
@@ -5,5 +6,5 @@ namespace TaskManagementApi.Application.Common.Interfaces.Repository;
 public interface IGetDomainTaskRepository
 {
     Task<ResponseType<Guid>> GetCurrentUserDomainIdCreateTaskAsync();
-    Task<ResponseType<Guid>> GetCurrentUserDomainIdUpdateTaskAsync(Guid id);
+    Task<ResponseType<TaskItem>> GetCurrentUserDomainIdUpdateTaskAsync(Guid id);
 }
