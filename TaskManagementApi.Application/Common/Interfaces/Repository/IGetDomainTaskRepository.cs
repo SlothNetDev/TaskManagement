d95daee0de20c9d507c58d5134ebad3410e60f65
slothnetdev <sungshigun@gmail.com>
2025-07-18 05:34:25 +0800

    feat: Adding GetCurrentUserDomainIdGetAllTaskAsync, GetCurrentUserDomainIdGetByIdTaskAsync, GetCurrentUserDomainIdPaganationTaskAsync, GetCurrentUserDomainIdSearchTaskAsync
    in IGetDomainTaskRepository

--- TaskManagementApi.Application/Common/Interfaces/Repository/IGetDomainTaskRepository.cs
+++ TaskManagementApi.Application/Common/Interfaces/Repository/IGetDomainTaskRepository.cs
@@ -8,4 +8,12 @@ public interface IGetDomainTaskRepository
     Task<ResponseType<Guid>> GetCurrentUserDomainIdCreateTaskAsync();
     Task<ResponseType<TaskItem>> GetCurrentUserDomainIdUpdateTaskAsync(Guid id);
     Task<ResponseType<TaskItem>> GetCurrentUserDomainIdDeleteTaskAsync(Guid id);
+    
+    
+    Task<ResponseType<Guid>> GetCurrentUserDomainIdGetAllTaskAsync();
+    Task<ResponseType<TaskItem>> GetCurrentUserDomainIdGetByIdTaskAsync(Guid id);
+    Task<ResponseType<TaskItem>> GetCurrentUserDomainIdPaganationTaskAsync(Guid id);
+    Task<ResponseType<TaskItem>> GetCurrentUserDomainIdSearchTaskAsync(string id);
+    
+    
 }
